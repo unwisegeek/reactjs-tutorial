1f46ee37a5a9a496f044904f1d9f89b0a3cee3ab
John Madon <john@unwisegeek.net>
2021-10-03 07:11:41 -0400

    Testing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => { this.setState({value: 'X'}) }}>
+          {this.props.value}
         </button>
       );
     }
